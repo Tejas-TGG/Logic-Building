401c858e265cf1b0166ec6e74a07937faa816672
Tejas Gawande <102826230+Tejas-TGG@users.noreply.github.com>
2023-04-07 20:43:13 +0530

    Add files via upload

---
+++ Problems-on-Bitwise/problem49.java
@@ -0,0 +1,49 @@
+//  Accept two number from user and count number of common on bits of that number
+
+
+
+
+import java.lang.*;
+import java.util.*;
+
+
+public class program350
+{
+    public static void main(String args[])
+    {
+        Scanner sobj=new Scanner(System.in);
+        System.out.println("Enter number:");
+        int iNo1=sobj.nextInt();
+
+        System.out.println("Enter number:");
+        int iNo2=sobj.nextInt();
+
+        int iNo=iNo1 & iNo2 ;
+
+        int iMask=0x00000001;
+        int iResult=0;
+        int iCnt=0;
+
+        for(int i=1 ; i <=32 ; i++)
+        {
+            iResult= iNo & iMask;
+
+            if(iResult==iMask)
+            {
+                
+                iCnt++;
+            }
+
+            iMask=iMask<<1;
+
+        }
+
+        System.out.println("Number of common on bits are "+iCnt);
+
+
+
+        
+
+    }
+    
+}
